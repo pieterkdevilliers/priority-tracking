2d25c714174e1e2d16339b0b89ccb3026a7eb6ee
pieterkdevilliers <pieterkdevilliers@me.com>
2022-12-28 11:34:58 +0000

    Add initial models for actions, priorities and categories

--- actions/admin.py
+++ actions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Action, Category, Priority
 
 # Register your models here.
+admin.site.register(Action)
+admin.site.register(Category)
+admin.site.register(Priority)
--- actions/models.py
+++ actions/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, null=False, blank=False)
+    description = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
+
+
+class Priority(models.Model):
+    title = models.CharField(max_length=50, null=False, blank=False)
+    description = models.CharField(max_length=150)
+    activeStatus = models.BooleanField(null=False, blank=False, default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Action(models.Model):
+    title = models.CharField(max_length=50, null=False, blank=False)
+    trackedTime = models.DurationField(null=True, blank=True)
+    doneStatus = models.BooleanField(null=False, blank=False, default=False)
+    date = models.DateField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False, blank=False)
+    priority = models.ForeignKey(Priority, on_delete=models.CASCADE, null=False, blank=False)
+
+    def __str__(self):
+        return self.title
+
