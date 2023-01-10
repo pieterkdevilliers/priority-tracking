700feffb7888eccb671f1dff65b3a83187516ccc
pieterkdevilliers <pieterkdevilliers@me.com>
2023-01-10 13:47:21 +0000

    Remove import env from settings.py

--- priority_tracker/settings.py
+++ priority_tracker/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
