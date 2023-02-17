2d811d69431d196080224956cdb543d0bbc156bd
pieterkdevilliers <pieterkdevilliers@me.com>
2023-02-17 07:55:00 +0000

    Refine filtered_action_reload process

--- static/js/script.js
+++ static/js/script.js
@@ -36,6 +36,17 @@ else {
 }});
 
 
+// // Resets the action_date query when a new query is started
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById(
+        "get-filtered-actions").addEventListener("click", resetQuery);
+    
+        function resetQuery() {
+            document.cookie = "action_date=; expires=Thu, 02 Jan 1970 00:00:01 GMT;";
+        }
+    });
+
+
 // Sets and holds the action-date query for filtered-actions
 // in order to reload the page with the same query
 window.addEventListener('DOMContentLoaded', (event) => {
@@ -52,6 +63,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
             const urlParams = new URLSearchParams(relistQuery);
             const relistParam = urlParams.get('action_date');
             document.cookie = 'action_date=' + relistParam;
+            if (reload_required_status == 1) {
+                resetReloadRequiredStatus();
+                window.location.href = "?action_date=" + lastQuery;
+                
+                }
         }
         else if
             (undo_filtered_action == null && relist_filtered_action == null) {
@@ -63,27 +79,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
                     }
                 }
             }
-        else if
-            (undo_filtered_action == null || relist_filtered_action == null) {
-                lastQuery = retrieveLastQuery()
-                if (lastQuery != null) {
-                    if (reload_required_status == 0) {
-                    window.location.href = "?action_date=" + lastQuery;
-                    setReloadRequiredStatus();
-                    }
-                }
-            }
     }});
-
-// // Resets the action_date query when a new query is started
-window.addEventListener('DOMContentLoaded', (event) => {
-document.getElementById(
-    "get-filtered-actions").addEventListener("click", resetQuery);
-
-    function resetQuery() {
-        document.cookie = "action_date=; expires=Thu, 02 Jan 1970 00:00:01 GMT;";
-    }
-});
 
 
 // Resets the reload_required_status when an action is completed, undone
@@ -100,7 +96,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         "relist-filtered-action");
         if (relist_filtered_action != null) {
             document.getElementById(
-            "complete-filtered-action").addEventListener(
+            "relist-filtered-action").addEventListener(
             "click", resetReloadRequiredStatus);
             };
     var undo_filtered_action = document.getElementById(
@@ -112,30 +108,33 @@ window.addEventListener('DOMContentLoaded', (event) => {
             };
     });
 
+
+// Resets the reload_required_status to 0
 function resetReloadRequiredStatus() {
     document.cookie = 'reload_required_status=' + '0';
 }
 
 
+// Sets the reload_required_status to 1
 function setReloadRequiredStatus() {
     document.cookie = 'reload_required_status=' + '1';
 }
 
 
-// Retrieves the lates action_date value from cookies
-function retrieveLastQuery() {
+// Retrieves the last reload_required_status value from cookies
+function retrieveReloadRequiredStatus() {
     const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${"action_date"}=`);
+    const parts = value.split(`; ${"reload_required_status"}=`);
     if (parts.length == 2){
         return parts.pop().split(';').shift();
     }
 }
 
 
-// Retrieves the lates reload_required_status value from cookies
-function retrieveReloadRequiredStatus() {
+// Retrieves the last action_date value from cookies
+function retrieveLastQuery() {
     const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${"reload_required_status"}=`);
+    const parts = value.split(`; ${"action_date"}=`);
     if (parts.length == 2){
         return parts.pop().split(';').shift();
     }
@@ -146,4 +145,4 @@ setTimeout(function(){
     if ($('#message').length > 0) {
         $('#message').remove();
     }
-    }, 2000);
+    }, 1000);
